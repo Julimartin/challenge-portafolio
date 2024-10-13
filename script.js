539f5a8750edc472daa64b8c0daac4ddd8e7ddd5
Julimartin <martin.julieta@hotmail.com>
2024-10-13 14:02:50 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//La validación del formulário con javascript es considerada un desafío extra, no es obligatório realizar esta validación para realizar su entrega
+
+
+// Función para alternar la visibilidad del menú en versión mobile
+function toggleMenu() {
+    const menu = document.getElementById('navMenu');
+    menu.classList.toggle('show');
+}
+
+// Función para cerrar el menú al hacer clic en un enlace en versión mobile
+function closeMenu() {
+    const navMenu = document.getElementById("navMenu");
+}
